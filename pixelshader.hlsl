62fb556fb8bdc314e2a233ca09c386f24aadb0c0
marcoigorr <petronio.mrc@gmail.com>
2023-03-28 00:08:23 +0200

    code for discarding black pixels in texture

--- pixelshader.hlsl
+++ pixelshader.hlsl
@@ -15,5 +15,11 @@ SamplerState objSamplerState : SAMPLER : register(s0);
 float4 main(PS_INPUT input) : SV_TARGET
 {
     float3 pixelColor = objTexture.Sample(objSamplerState, input.inTexCoord);
+    
+    if (pixelColor.r == 0.0f && pixelColor.g == 0.0f && pixelColor.b == 0.0f)
+    {
+        discard;
+    }
+    
     return float4(pixelColor, alpha);
 }
